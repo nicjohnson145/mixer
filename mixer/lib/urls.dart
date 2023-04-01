ff6631d230f50ed1e336bd1e907cda725d760819
Nic Johnson <nicjohnson145@hotmail.com>
2023-04-01 16:34:38 -0500

    fix: correct url

--- mixer/lib/urls.dart
+++ mixer/lib/urls.dart
@@ -3,7 +3,7 @@ import 'dart:io' show Platform;
 import 'package:fixnum/fixnum.dart';
 
 class Urls {
-    static const String liveBaseURL = "https://mixer.nicjohnson.info";
+    static const String liveBaseURL = "https://new-mixer.nicjohnson.info";
     static final String localBaseURL = Platform.isIOS ? "http://127.0.0.1:8080" : "http://10.0.2.2:8080";
     static final String base = Foundation.kReleaseMode ? liveBaseURL : localBaseURL;
     static final String apiv1 = base + "/api/v1";
